a5d431ce7517f9085a823619e78e23bb515c9303
Mark M <chtozamm@gmail.com>
2023-08-19 13:35:17 +0300

    remote images setup

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'static-maps.yandex.ru'
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig
