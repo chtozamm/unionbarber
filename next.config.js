9b2ef8a3701d29d34d15a896cf071109121a7581
Mark M <chtozamm@gmail.com>
2023-08-19 13:59:07 +0300

    remove remote images patterns

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'static-maps.yandex.ru'
-            }
-        ]
-    }
-}
+const nextConfig = {}
 
 module.exports = nextConfig
