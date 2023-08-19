5065503ca8b6c9361f7957ece3eb1bfc8265e971
Mark M <chtozamm@gmail.com>
2023-08-19 15:11:59 +0300

    opengraph, twitter image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,18 @@ export const metadata: Metadata = {
   title: "Барбершоп Юнион | Union Barbershop",
   description:
     "Мужская парикмахерская в Пскове с атмосферой классического барбершопа. Стрижем, бреем, создаем стиль.",
+  openGraph: {
+    title: "Барбершоп Юнион | Union Barbershop",
+    description:
+      "Мужская парикмахерская в Пскове с атмосферой классического барбершопа. Стрижем, бреем, создаем стиль.",
+    url: "https://www.unionbarber.ru",
+  },
+  twitter: {
+    title: "Барбершоп Юнион | Union Barbershop",
+    description:
+      "Мужская парикмахерская в Пскове с атмосферой классического барбершопа. Стрижем, бреем, создаем стиль.",
+    card: "summary_large_image",
+  },
 };
 
 export default function RootLayout({
