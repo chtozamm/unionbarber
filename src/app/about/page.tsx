d45c946464257625bcf9f1a94e64782d973dd0a2
Mark M <chtozamm@gmail.com>
2023-08-19 13:23:06 +0300

    info about page under construction

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,4 @@
+import { ConstructionIcon, HardHatIcon, Info } from "lucide-react";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -7,7 +8,14 @@ export const metadata: Metadata = {
 export default function AboutPage() {
   return (
     <main className="flex flex-col items-center gap-6 lg:gap-12 mt-4 max-w-[1100px] mx-auto px-4 md:px-6">
-      <div className="aspect-video w-full"></div>
+      <section className="w-screen md:w-full border-t">
+        <div className="aspect-video w-full flex flex-col justify-around items-center">
+          <ConstructionIcon className="inline-block h-24 w-24" />
+          <p className="flex justify-center gap-1.5 items-center text-sm text-zinc-500">
+            <Info className="w-4 h-4" /> Раздел в разработке
+          </p>
+        </div>
+      </section>
     </main>
   );
 }
