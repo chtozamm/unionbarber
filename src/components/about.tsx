e0b47009b13f6fe1c04d219a0191c1698669e02b
Mark M <chtozamm@gmail.com>
2023-08-24 15:04:50 +0300

    fixed gap on mobile

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -6,7 +6,7 @@ export default function About() {
   return (
     <section className="w-screen md:w-full sm:mb-6">
       <div className="sm:grid sm:grid-cols-2 sm:gap-3">
-        <div className="flex flex-col gap-6 sm:mt-6 text-sm px-6 sm:col-start-1 sm:col-end-1 sm:row-span-1">
+        <div className="flex flex-col sm:mt-6 text-sm px-6 sm:col-start-1 sm:col-end-1 sm:row-span-1">
           <h3 className="text-xl md:text-2xl font-semibold tracking-tight">
             О нас:
           </h3>
